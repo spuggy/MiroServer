af000cd93a106387b8e1a874b3e58bcdc47a55c3
Richard Spence <richard.spence@gmail.com>
2012-03-25 10:47:12 +0100

    added start chart and added in a few sundry files.  Also excluded miro/out directory

---
+++ src/service/uk/co/bluetrail/miro/MiroStarChart.java
@@ -0,0 +1,72 @@
+package uk.co.bluetrail.miro;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileOutputStream;
+
+import javax.imageio.ImageIO;
+
+public class MiroStarChart {
+
+
+
+	int[] x = {190,305,356,305,190, 66, 30,66};
+	int[] y = {30, 67, 180,289,322,289,180,63};
+	String IMG_NAME = "miro.jpg";
+	String filePath = "";
+	Color textColor = null;
+	
+	//images start from 12 oclock
+	
+	public MiroStarChart(String filePath,  Color textColor) {
+	
+		this.filePath = filePath;
+		this.textColor = textColor;
+	}
+	
+	public void createChart(String outputFileName, int[] values) throws Exception {
+
+		if(values.length!=8) {
+			throw new RuntimeException("You need 8 values");
+		}
+		
+		
+        BufferedImage image = null;
+        String filePath = this.filePath + "/images/miroteamreport/miroStartChartBackground.png";
+        File file = new File(filePath);
+        image = ImageIO.read(file);
+        Graphics2D g = (Graphics2D)image.getGraphics();
+
+        for(int i = 0;i < values.length;i++) {
+            
+            g.setFont(new Font("Calibri", Font.BOLD, 25));
+            g.setColor(this.textColor);
+            FontMetrics fm = g.getFontMetrics(g.getFont());
+            
+            g.drawString(values[i]+"", x[i] , y[i] );
+        }
+        
+        g.dispose();
+        
+
+		char seperator = File.separatorChar;
+
+		String outputFile = new String(this.filePath+seperator+"out"+seperator+outputFileName);
+
+		FileOutputStream out = new FileOutputStream(outputFile);
+
+        ImageIO.write(image, "jpg", out);
+        out.close();
+        
+       
+    }
+	
+	
+	
+
+}
---
+++ test/service/uk/co/bluetrail/miro/TestMiroStarChart.java
@@ -0,0 +1,35 @@
+package uk.co.bluetrail.miro;
+
+import java.awt.Color;
+import java.io.File;
+
+import junit.framework.TestCase;
+
+public class TestMiroStarChart extends TestCase {
+	
+	public void testGenerate() {
+		
+		int[] values = {10,20,30,40,50,60,70,80};
+		
+		String filePath = "/Users/Richard/Documents/workspace-copy/MiroServer3/web/miro";
+		
+		MiroStarChart msc = new MiroStarChart(filePath, Color.BLUE);
+		
+		String fileName = "arg.png" ;
+		
+		try {
+			msc.createChart(fileName, values);
+		} catch (Exception e) {
+			fail("Exception createTeamChart " + e.toString());
+		}
+		
+		File f = new File(filePath+"/out/"+fileName);
+		
+		if(!f.exists()) {
+			fail("file no found");
+		}
+		
+		
+	}
+
+}
