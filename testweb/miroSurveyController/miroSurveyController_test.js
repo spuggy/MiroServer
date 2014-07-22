7b9bf63eefd58358730a8fa2dbcc874929098375
spugamola <richard.spence@gmail.com>
2014-07-22 20:43:49 +0100

    made db-load and db-prepare work

--- testweb/miroSurveyController/miroSurveyController_test.js
+++ testweb/miroSurveyController/miroSurveyController_test.js
@@ -1,5 +1,11 @@
 describe('miroSurveyController', function(){
   describe('create', function(){
+    it('should create the controller', function(){
+      var  msc = new MiroSurveyController(1,2) ;
+      expect(msc).exist;
+
+    })
+
     it('should return the length', function(){
       var  msc = new MiroSurveyController(1,2) ;
       expect(msc).exist;
